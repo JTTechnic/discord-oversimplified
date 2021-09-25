a958e9761f8bba7d70406db712ec451c37790fe7
Jaron <jaron_zijlstra@outlook.com>
2021-09-25 13:40:05 +0200

    feat: make variables readonly

--- src/VariableManager.js
+++ src/VariableManager.js
@@ -14,6 +14,7 @@ module.exports = class VariableManager {
 	constructor(interpeter) {
 		/**
 		 * @type {Collection<string, Variable>}
+		 * @readonly
 		 */
 		this.variables = new Collection();
 		Object.values(requireAll(join(__dirname, "variables")))
