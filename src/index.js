7b372a98e08061fdd4446ed9ee686cc548f6e19f
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-09-28 11:50:40 +0200

    feat: add interpreter (#9)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@
 
 module.exports = {
 	Client: require("./Client"),
-	Builder: require("./Builder"),
-	Interpeter: require("./Interpeter"),
-	Variable: require("./variable/Variable"),
-	VaraibleManager: require("./variable/VariableManager")
+	Builder: require("./Builder")
 };
---
+++ src/interpreter/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+	Environment: require("./Environment"),
+	parse: require("./parsing/parse"),
+	evaluate: require("./evaluation/evaluate")
+};
