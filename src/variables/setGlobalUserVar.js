d9fd963fddd2fa2775d1a7a9e19bb36af50fe1b3
Jaron <jaron_zijlstra@outlook.com>
2021-09-28 16:56:19 +0200

    feat: added setglobaluservar variable

---
+++ src/variables/setGlobalUserVar.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const Variable = require("../Variable");
+
+module.exports = class SetGlobalUserVar extends Variable {
+	constructor(client) {
+		super(client, "setglobaluservar");
+		this.setDefinition((name, value, user) => {
+			const vars = this.client.databases.globalUserVars.get(user) ?? {};
+			vars[name] = value;
+			this.client.databases.globalUserVars.set(user, vars);
+		});
+	}
+};
