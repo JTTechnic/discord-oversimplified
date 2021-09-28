a525c4a5322efd8a58f4c56375226b4539a126f2
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-09-28 12:03:52 +0200

    fix: fixed typings (#10)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,53 +1,34 @@
 import Dext = require("discord-extend");
-import {CommandInteraction, MessageEmbed, Collection} from "discord.js";
+import {CommandInteraction} from "discord.js";
 
-export interface ClientOptions extends Dext.ClientOptions {
+interface ClientOptions extends Dext.ClientOptions {
 	token: string;
-	customVariables?: string | typeof Variable[];
+	customVariables?: string;
 }
 
-export class Client extends Dext.Client {
+declare class Environment {
+	public constructor(parent: Environment);
+	extend(): Environment;
+	lookup(name: string): Environment;
+	get(name: string): any;
+	set(name: string, value: any): any;
+	define(name: string, value: any): any;
+}
+
+declare class Client extends Dext.Client {
+	private readonly environment: Environment;
 	public constructor(options: ClientOptions);
 	public command(trigger: string, code: string): void;
 	public commandsIn(dir: string): void;
 	private _validateCommand(trigger: string, code: string): void;
+	private _initEnvironment(): void;
 }
 
-export class Builder {
+declare class Builder {
 	public static command(
 		options: Dext.SlashSubCommandInfo,
 		runMethod: (interaction: CommandInteraction) => any
 	): Dext.SlashSubCommand;
 }
 
-export abstract class Variable {
-	public constructor(interpeter: Interpeter, name: string, hiddenProperties: string[]);
-	public readonly interpeter: Interpeter;
-	public readonly name: string;
-	public readonly hiddenProperties: string[];
-}
-
-export class VariableManager {
-	public constructor(client: Client, interpeter: Interpeter);
-	public readonly variables: Collection<string, Variable>;
-}
-
-type VariableResolvable = Variable | Function | null;
-
-export class Interpeter {
-	public readonly variables: {
-		embeds: MessageEmbed[];
-		content: string | null;
-	};
-	protected readonly variableManager: VariableManager;
-	public constructor(client: Client);
-	public interpet(code: string): this;
-	private getLines(code: string): string[];
-	private getTokens(line: string): string[];
-	private getVariable(tokens: string[]): {
-		variable: VariableResolvable;
-		path: string[];
-		params: string[];
-	};
-	private isValidVariable(variable: VariableResolvable): variable is (Variable & {construct: Function}) | Function;
-}
+export {ClientOptions, Client, Builder};
