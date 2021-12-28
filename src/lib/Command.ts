50b2f9f5df120f5deecc315db520be89d4a58fd1
Jaron <jaron_zijlstra@outlook.com>
2021-12-28 20:46:46 +0100

    feat: add cooldowns

--- src/lib/Command.ts
+++ src/lib/Command.ts
@@ -1,16 +1,22 @@
 import { evaluate, parse } from "@discordextend/interpreter";
 import { Command as SapphireCommand, container, Events } from "@sapphire/framework";
-import { Collection, CommandInteraction } from "discord.js";
+import type { CommandInteraction, Snowflake } from "discord.js";
+import { Collection } from "@discordjs/collection";
 import type { Client } from "./Client";
 import { Util } from "./Util";
 
 export interface CommandOptions {
 	trigger: string[];
 	code: string;
+	cooldown?: number;
+	globalCooldown?: number;
+	cooldownError?: string;
 }
 
 export class Command extends SapphireCommand {
 	private readonly commands: Collection<string, (interaction: CommandInteraction) => any> = new Collection();
+	private readonly cooldowns: Collection<string, Collection<Snowflake, number>> = new Collection();
+	private readonly globalCooldowns: Collection<string, number> = new Collection();
 
 	public constructor(public readonly client: Client, options: CommandOptions) {
 		super(
@@ -35,12 +41,46 @@ export class Command extends SapphireCommand {
 		return `${trigger[0]}${this.capitalize(trigger[1])}${this.capitalize(trigger[2])}`;
 	}
 
-	public addCommand({ trigger, code }: CommandOptions) {
-		this.commands.set(this.getTriggerName(trigger), (interaction) => {
+	public addCommand({ trigger, code, cooldown = 0, globalCooldown = 0, cooldownError }: CommandOptions) {
+		const triggerName = this.getTriggerName(trigger);
+		this.commands.set(triggerName, (interaction) => {
+			console.log("Time to check cooldowns");
+
+			// Cooldown check
+			const { id: userId } = interaction.user;
+			const cooldowns = this.cooldowns.ensure(triggerName, () => new Collection());
+			const timeleft = Math.max(this.getCooldown(this.globalCooldowns, userId), this.getCooldown(cooldowns, userId));
+			if (timeleft > 0) {
+				return interaction.reply({
+					content:
+						cooldownError ?? `You must wait ${Math.ceil(timeleft / 1000)} seconds before using this command again.`,
+					ephemeral: true
+				});
+			}
+
+			console.log("Time to execute!");
+
+			// Command execution
 			container.environment.define("messageoptions", {});
 			Util.setInteractionVariables(interaction);
 			evaluate(parse(code), container.environment);
+
+			// Cooldown set
+			const now = Date.now();
+			this.globalCooldowns.set(userId, now + globalCooldown);
+			return cooldowns.set(userId, now + cooldown);
 		});
+	}
+
+	private getCooldown(cooldowns: Collection<Snowflake, number>, userId: Snowflake) {
+		if (cooldowns.has(userId)) {
+			const cooldown = cooldowns.get(userId);
+			const now = Date.now();
+			if (cooldown && cooldown > now) {
+				return cooldown - now;
+			}
+		}
+		return 0;
 	}
 
 	public override chatInputRun(interaction: CommandInteraction) {
