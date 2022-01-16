2540f7535b396daeff196bd727d8fa4728c5d7ed
Jaron <jaron_zijlstra@outlook.com>
2022-01-16 14:34:26 +0100

    fix: export database and variable

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./lib/container";
 export * from "./lib/Client";
-export * from "./lib/Database";
+export * from "./lib/structures/Database";
+export * from "./lib/structures/Variable";
 export * from "./lib/Util";
-export * from "./lib/Variable";
