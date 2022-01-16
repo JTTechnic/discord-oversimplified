7d1f91f0b28a690f2974da2de6640550953210ee
Jaron <jaron_zijlstra@outlook.com>
2022-01-16 18:09:26 +0100

    fix(stores): export stores
    export stores so entries can be changed

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import "./lib/augment";
 
 export * from "./lib/Client";
 export * from "./lib/structures/Database";
+export * from "./lib/structures/DatabaseStore";
 export * from "./lib/structures/Variable";
+export * from "./lib/structures/VariableStore";
