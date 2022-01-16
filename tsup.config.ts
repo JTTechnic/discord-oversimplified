ba9d86911a69b9f3e41af20947c94dadb4ce4f0d
Jaron <jaron_zijlstra@outlook.com>
2022-01-16 14:35:45 +0100

    fix: tsup entry points

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import type { Options } from "tsup";
 export const tsup: Options = {
 	clean: true,
 	dts: false,
-	entryPoints: ["src/index.ts"],
+	entryPoints: ["src/index.ts", "src/lib/structures", "src/lib/databases", "src/lib/variables"],
 	format: ["esm", "cjs"],
 	minify: true,
 	skipNodeModulesBundle: true,
