4aadf16d5c89456fc7f4d84c3882e3b44b76b2a5
Jaron <jaron_zijlstra@outlook.com>
2022-01-16 14:33:56 +0100

    fix: register stores

--- src/lib/container.ts
+++ src/lib/container.ts
@@ -1,68 +1,14 @@
 import { container } from "@sapphire/framework";
-import { Database } from "./Database";
 import { Environment } from "@discordextend/interpreter";
-import { ContentVariable } from "./variables/ContentVariable";
-import { EmbedVariable } from "./variables/EmbedVariable";
-import { GetGlobalUserVarVariable } from "./variables/GetGlobalUserVarVariable";
-import { GetUserVarVariable } from "./variables/GetUserVarVariable";
-import { GetVarVariable } from "./variables/GetVarVariable";
-import { SetGlobalUserVarVariable } from "./variables/SetGlobalUserVarVariable";
-import { SetUserVarVariable } from "./variables/SetUserVarVariable";
-import { SetVarVariable } from "./variables/SetVarVariable";
-import { Util } from "./Util";
-import { DeleteVariable } from "./variables/DeleteVariable";
-import { HasPermVariable } from "./variables/HasPermVariable";
-import { KickVariable } from "./variables/KickVariable";
-import { BanVariable } from "./variables/BanVariable";
-import { UnbanVariable } from "./variables/UnbanVariable";
-import { IsOwnerVariable } from "./variables/IsOwnerVariable";
+import { DatabaseStore } from "./structures/DatabaseStore";
+import { VariableStore } from "./structures/VariableStore";
 
 declare module "@sapphire/pieces" {
 	export interface Container {
-		databases: {
-			vars: Database;
-			userVars: Database<{
-				[user: string]:
-					| {
-							[key: string]: any;
-					  }
-					| undefined;
-			}>;
-			globalUserVars: Database<{
-				[key: string]: any;
-			}>;
-		};
 		environment: Environment;
 	}
 }
 
-container.databases = {
-	vars: new Database("vars"),
-	userVars: new Database<{
-		[user: string]:
-			| {
-					[key: string]: any;
-			  }
-			| undefined;
-	}>("uservars"),
-	globalUserVars: new Database<{
-		[key: string]: any;
-	}>("globaluservars")
-};
+container.stores.register(new DatabaseStore());
+container.stores.register(new VariableStore());
 container.environment = new Environment();
-Util.defineVariable(
-	new ContentVariable(),
-	new EmbedVariable(),
-	new GetGlobalUserVarVariable(),
-	new GetUserVarVariable(),
-	new GetVarVariable(),
-	new SetGlobalUserVarVariable(),
-	new SetUserVarVariable(),
-	new SetVarVariable(),
-	new DeleteVariable(),
-	new HasPermVariable(),
-	new KickVariable(),
-	new BanVariable(),
-	new UnbanVariable(),
-	new IsOwnerVariable()
-);
