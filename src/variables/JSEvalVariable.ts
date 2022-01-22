d6b595f8dadb68d098c6ce8f7152438899f97d0e
Jaron <jaron_zijlstra@outlook.com>
2022-01-22 17:15:21 +0100

    feat: add jseval and update eval

--- src/variables/JSEvalVariable.ts
+++ src/variables/JSEvalVariable.ts
@@ -1,14 +1,13 @@
-import { evaluate, parse } from "@discordextend/interpreter";
 import type { PieceContext } from "@sapphire/framework";
 import { Variable, VariableOptions } from "../lib/structures/Variable";
 
-export class EvalVariable extends Variable {
+export class JSEvalVariable extends Variable {
 	public constructor(context: PieceContext, options: VariableOptions) {
 		super(context, {
 			...options,
-			name: "eval",
+			name: "jseval",
 			// eslint-disable-next-line no-eval
-			definition: (code: string) => evaluate(parse(code), this.container.environment)
+			definition: eval
 		});
 	}
 }
